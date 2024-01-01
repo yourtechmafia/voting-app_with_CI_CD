98dd4195a2b84d0440a9aad1e1fe84851bc8218a
Ismail Ajagbe <ismailajagbe@gmail.com>
2024-01-01 17:43:28 +0100

    Modified terraform directory

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cluster_endpoint" {
-  value = aws_eks_cluster.cluster.endpoint
-}
-
-output "cluster_id" {
-  value = aws_eks_cluster.cluster.id
-}
--- terraform/provider.tf
+++
@@ -1,6 +0,0 @@
-  provider "aws" {
-    region = var.region
-    access_key = "YOUR ACCESS KEY"
-    secret_key = "YOUR SECRET KEY"
-  }
-
