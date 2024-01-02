913543c456bfc3a367c8adbd98594b12ebb7be6e
Ismail Ajagbe <ismailajagbe@gmail.com>
2024-01-02 12:54:18 +0100

    Update ebs-csi-driver.tf

--- terraform/ebs-csi-driver.tf
+++ terraform/ebs-csi-driver.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name            = aws_eks_cluster.eks.name
   addon_name              = "aws-ebs-csi-driver"
-  addon_version           = "v1.2.0"  // Replace with the required version
+  addon_version           = "v1.5.2-eksbuild.1"  // Replace with the required version
   service_account_role_arn = aws_iam_role.ebs_csi_role.arn
 }
