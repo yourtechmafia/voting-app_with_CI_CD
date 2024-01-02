8f2528dd998c3864c0bbc3ee781374c31b66d4e8
Ismail Ajagbe <ismailajagbe@gmail.com>
2024-01-02 10:45:07 +0100

    Update eks-nodes.tf

--- terraform/eks-nodes.tf
+++ terraform/eks-nodes.tf
@@ -25,7 +25,5 @@ resource "aws_eks_node_group" "node-grp" {
       aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
       aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
       aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    ]labels = {
-      env = "dev"
-    }
+    ]
 }
