c41f489aaef80e8f024f9792605626796a573e32
Ismail Ajagbe <ismailajagbe@gmail.com>
2024-01-02 14:50:44 +0100

    Update oidc-provider.tf

--- terraform/oidc-provider.tf
+++ terraform/oidc-provider.tf
@@ -1,3 +1,9 @@
-data "aws_iam_openid_connect_provider" "oidc_provider" {
-  url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+data "external" "oidc_thumbprint" {
+  program = ["bash", "${path.module}/get-thumbprint.sh", aws_eks_cluster.eks.identity[0].oidc[0].issuer]
+}
+
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
+  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.external.oidc_thumbprint.result["thumbprint"]]
 }
