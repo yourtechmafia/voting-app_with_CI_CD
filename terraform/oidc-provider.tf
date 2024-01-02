e9436bfc8119fca1a566921ad44c5d4f476bb8ec
Ismail Ajagbe <ismailajagbe@gmail.com>
2024-01-02 13:47:02 +0100

    Update oidc-provider.tf

--- terraform/oidc-provider.tf
+++ terraform/oidc-provider.tf
@@ -1,3 +1,3 @@
-data "aws_iam_openid_connect_provider" "existing_oidc_provider" {
-  arn = "arn:aws:iam::446319600542:oidc-provider/oidc.eks.eu-west-2.amazonaws.com/id/5D451DCE6B903B90F76ECE821CA309A0"  //  Replace with your OIDC provider's ARN
+data "aws_iam_openid_connect_provider" "oidc_provider" {
+  url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
