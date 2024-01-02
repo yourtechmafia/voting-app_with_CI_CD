e0e734d9ec4edb7bf2a8c281fe3b57ee5f292b4b
Ismail Ajagbe <ismailajagbe@gmail.com>
2024-01-02 12:57:06 +0100

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_cluster" "eks" {
   name     = "votingapp-eks"
   role_arn = aws_iam_role.master.arn
+  version  = "1.24" //  Version 1.28 as of this date (Jan 2, 2024) is buggy
 
   vpc_config {
     subnet_ids = [aws_subnet.Mysubnet01.id, aws_subnet.Mysubnet02.id]
